d9f3b017c9a57ac734617fe7dc5654ae7b61eb03
Bahram Aghaei <aghaee.bahram@gmail.com>
2019-01-17 11:49:26 +0330

    by using regex

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -1,11 +1,6 @@
 import re
-from string import ascii_lowercase
 
 
 def abbreviate(words):
-    words = words.replace('_', ' ', -1).replace('-', ' ', -1)
-    return ''.join([
-        word[0].upper()
-        for word in words.lower().split()
-        if word[0] in ascii_lowercase
-    ])
+    ACRONYM_RE = re.compile(r'\b[-_]?([a-zA-Z])')
+    return ''.join(map(lambda c: c.upper(), ACRONYM_RE.findall(words.replace("'", ''))))
