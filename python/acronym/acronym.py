550814ebc8bf6931878ae98e7ec993d3d5ba8797
Bahram Aghaei <aghaee.bahram@gmail.com>
2019-01-17 17:18:10 +0330

    acronym: make it better

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -2,5 +2,5 @@ import re
 
 
 def abbreviate(words):
-    ACRONYM_RE = re.compile(r'\b[-_]?([a-zA-Z])')
-    return ''.join(map(lambda c: c.upper(), ACRONYM_RE.findall(words.replace("'", ''))))
+    ACRONYM_RE = re.compile(r"\b[-_]?([a-zA-Z])[a-zA-Z']*")
+    return ''.join(ACRONYM_RE.findall(words)).upper()
