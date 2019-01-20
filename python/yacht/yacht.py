fd4dd2ef977c882c9b6392644d889e88802d39dc
GreatBahram <aghaee.bahram@gmail.com>
2019-01-20 13:10:23 +0330

    add yacht solution

--- python/yacht/yacht.py
+++ python/yacht/yacht.py
@@ -16,23 +16,33 @@ CHOICE = 11
 
 
 def first_sixth(number, counter):
-    print(counter.items())
-    return number * len([value for key, value in counter.items() if key == number])
+    return number * counter[number]
+
 
 def full_house(counter):
-    return sum([key * value for key, value in counter.items()])
+    if len(counter) == 2 and counter.most_common()[0][1] == 3:
+        return sum([key * value for key, value in counter.items()])
+    return 0
+
 
 def four_of_a_kind(counter):
-    return sum([key * value for key, value in counter.items() if key == 4])
+    most_common , count = counter.most_common()[0]
+    if count >= 4:
+        return most_common * 4
+    return 0
 
-def little_straight():
-    return 30
 
-def big_straight():
-    return 30
+def little_straight(counter):
+    return 30 if sorted(counter.keys()) == [1, 2, 3, 4, 5] else 0
+
+
+def big_straight(counter):
+    return 30 if sorted(counter.keys()) == [2, 3, 4, 5, 6] else 0
+
 
 def choice(counter):
     return sum([key * value for key, value in counter.items()])
+
 
 def yacht(counter):
     return 50 if len(counter) == 1 else 0
@@ -40,7 +50,7 @@ def yacht(counter):
 
 def score(dice, category):
     c = Counter(dice)
-    options = {
+    function = {
         YACHT : yacht,
         ONES : partial(first_sixth, 1),
         TWOS : partial(first_sixth, 2),
@@ -53,4 +63,5 @@ def score(dice, category):
         LITTLE_STRAIGHT : little_straight,
         BIG_STRAIGHT : big_straight,
         CHOICE : choice,
-    }.get(category)(c)
+    }.get(category)
+    return function(c)
