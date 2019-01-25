75c346e9e7b954ae8b9764ea8ce0f9b4ba4ae483
Bahram Aghaei <aghaee.bahram@gmail.com>
2019-01-25 20:56:20 +0330

    update phone-number regex

--- python/phone-number/phone_number.py
+++ python/phone-number/phone_number.py
@@ -2,16 +2,16 @@ import re
 
 NANP_RE = re.compile(r'''
     ^
-    \+?([0-9])?          # country code
-    [\s|\-|\.]*          # separator
-    \(?([2-9]\d{2})\)?   # area code
-    [\s|\-|\.]*          # separator
-    ([2-9]\d{2})         # exchange code
-    [\s|\-|\.]*          # separator
-    (\d{4})              # subscriber number
-    [\s|\-|\.]*          # separator
+    \+?1?                     # country code
+    \D*                       # separator
+    \(? ( [2-9] \d{2} ) \)?   # area code
+    \D*                       # separator
+    ( [2-9] \d{2} )           # exchange code
+    \D*                       # separator
+    ( \d{4} )                 # subscriber number
+    \D*                       # separator
     $
-    ''', re.VERBOSE)
+''', re.VERBOSE)
 
 
 class Phone:
@@ -22,16 +22,14 @@ class Phone:
         """Parse and extract phone number from string."""
         matched = NANP_RE.match(phone_number)
         if matched:
-            self.number = ''.join(matched.group(2, 3, 4))
-            self.country_code = matched.group(1)
-            self.area_code = matched.group(2)
-            self.exchange_code = matched.group(3)
-            self.subscriber_number = matched.group(4)
-
-            if self.country_code and self.country_code != '1':
-                raise ValueError("Phone number is not valid.")
+            self.number = ''.join(matched.group(1, 2, 3))
+            self.area_code = matched.group(1)
+            self.exchange_code = matched.group(2)
+            self.subscriber_number = matched.group(3)
         else:
             raise ValueError("Phone number is not valid.")
 
-    def pretty(self):
+    def __str__(self):
         return f'({self.area_code}) {self.exchange_code}-{self.subscriber_number}'
+
+    pretty = __str__
