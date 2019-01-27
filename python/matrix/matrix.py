273daeb7352ddcb5994bff75650c8cfeb58c23e1
Bahram Aghaei <aghaee.bahram@gmail.com>
2019-01-27 10:05:50 +0330

    make matrix even better than before

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,16 +1,27 @@
 class Matrix:
     def __init__(self, matrix_string):
-        self.from_string(matrix_string)
+        matrix = self.matrix_from_string(matrix_string)
+        transpose = self.transpose(matrix)
+        self.rows, self.columns = matrix, transpose
 
     def row(self, index):
-        return self.matrix[index-1]
+        return self.rows[index-1]
 
     def column(self, index):
-        return [row[index-1] for row in self.matrix]
+        return self.columns[index-1]
 
-    def from_string(self, string):
-        """Parse matrix from string input."""
-        self.matrix = [
+    @staticmethod
+    def matrix_from_string(string):
+        """Parse matrix from string."""
+        return [
             [int(item) for item in row.split()]
             for row in string.splitlines()
+        ]
+
+    @staticmethod
+    def transpose(matrix):
+        """Return a transposed version of a matrix."""
+        return [
+            list(column)
+            for column in zip(*matrix)
         ]
