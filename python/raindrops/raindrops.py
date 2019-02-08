0448e41bcf2c2c5a6f0f42605cc8a3217f1764b7
Bahram Aghaei <aghaee.bahram@gmail.com>
2019-02-08 15:03:46 +0330

    make it better

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -2,10 +2,16 @@ from collections import namedtuple
 
 Pair = namedtuple('Pair', ['factor', 'string'])
 
+DROPS_LIST = [
+    Pair(3, 'Pling'),
+    Pair(5, 'Plang'),
+    Pair(7, 'Plong'),
+]
 
-def raindrops(number):
+
+def raindrops(number: int) -> str:
     output = ''
-    for pair in [Pair(3, 'Pling'), Pair(5, 'Plang'), Pair(7, 'Plong')]:
+    for pair in DROPS_LIST:
         if number % pair.factor == 0:
             output += pair.string
     return output if output else str(number)
