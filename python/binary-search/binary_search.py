d3e63dd7e5753e7c8356c10c7b7f796db0c90163
Bahram Aghaei <aghaee.bahram@gmail.com>
2019-08-15 15:53:28 +0430

    add markdown and binary search

--- python/binary-search/binary_search.py
+++ python/binary-search/binary_search.py
@@ -1,12 +1,12 @@
 def find(search_list, value):
     low = 0
     high = len(search_list) - 1
-    while True:
+    while low <= high:
         mid = (low + high) // 2
-        if value > search_list[mid]:
+        if search_list[mid] < value:
             low = mid + 1
-        elif value < search_list[mid]:
+        elif search_list[mid] > value:
             high = mid - 1
         else:
-            True
-    return False
+            return mid
+    raise ValueError
